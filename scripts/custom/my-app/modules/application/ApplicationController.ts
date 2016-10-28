c205594c520bbde8a7fb9049a5d4e38dbc59a573
Dalibor Mesarić <dalibormesaric@users.noreply.github.com>
2016-10-28 14:00:55 +0200

    Initial commit

---
+++ scripts/custom/my-app/modules/application/ApplicationController.ts
@@ -0,0 +1,20 @@
+﻿/// <reference path="applicationservice.ts" />
+module myApp {
+    class ApplicationController {
+
+        constructor(
+            private applicationService: IApplicationService
+        ) {
+            var self = this;
+        }
+
+        private getApplicationName(): string {
+            var self = this;
+            return self.applicationService.getApplicationName();
+        }
+    }
+
+    myApp.controller("ApplicationController", [
+        "ApplicationService",
+        ApplicationController]);
+}
